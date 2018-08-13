d779e33b57fe1be54916eeb8d51e2a5fa7d1eedb
andysumi <andysumi@ruby.net.tokai-u.jp>
2018-08-13 18:52:12 +0900

    searchUserClassメソッドを追加

--- src/class.js
+++ src/class.js
@@ -2,7 +2,7 @@
   var ParseServerClient = (function () {
 
     function ParseServerClient(domain, appId, key) {
-      this.apiUrl = 'https://' + domain + '/parse';
+      this.apiUrl = 'https://' + domain + '/1';
       this.headers = {
         'X-Parse-Application-Id' : appId,
         'X-Parse-Master-Key'     : key,
@@ -14,6 +14,33 @@
       if (!key) throw new Error('"key"は必須です');
     }
 
+    ParseServerClient.prototype.searchUserClass = function (query, options) {
+      var params = [];
+
+      if (query) {
+        params.push('where=' + encodeURIComponent(JSON.stringify(query)));
+      }
+
+      if (options) {
+        for (var key in options) {
+          params.push(key + '=' + options[key]);
+        }
+      }
+
+      return this.fetch_(this.apiUrl + '/users?' + params.join('&'), {method: 'get'});
+    };
+
+    ParseServerClient.prototype.fetch_ = function (url, options) {
+      var response = UrlFetchApp.fetch(url, {
+        method             : options.method,
+        muteHttpExceptions : true,
+        contentType        : 'application/json; charset=utf-8',
+        headers            : this.headers,
+        payload            : JSON.stringify(options.payload) || {}
+      });
+
+      return JSON.parse(response.getContentText());
+    };
     return ParseServerClient;
   })();
 
--- src/main.js
+++ src/main.js
@@ -8,3 +8,14 @@
 function create(domain, appId, key) { // eslint-disable-line no-unused-vars
   return new ParseServerClient(domain, appId, key);
 }
+
+/**
+ * Userクラスを検索する
+ * https://docs.parseplatform.org/rest/guide/#querying
+ * @param {Object} query 【必須】クエリ、検索条件
+ * @param {Object} options 【任意】オプション ※ドキュメント参照
+ * @return {Object} 実行結果
+ */
+function searchUserClass(query, options) { // eslint-disable-line no-unused-vars
+  throw new Error('このメソッドは直接呼び出せません。createメソッドで取得したインスタンスより呼び出してください。');
+}
